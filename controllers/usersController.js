cf634260a866edef626eb0cb88216538ae51c52c
Belle <bebold.bebelle@gmail.com>
2018-10-20 12:52:19 -0400

    Successful test for the validation of the eamil already in use or not.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,27 +2,37 @@ const User = require("../models/user");
 
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log('UserController.signUp() called!');
+        // console.log('UserController.signUp() called!');
 
         const { email, password } = req.value.body;
         //The two lines below are equivalent of the line above.
         // const email = req.value.body.email;
         // const password = req.value.body.password
 
+        //Check if there is a user with the same email.
+        const foundUser = await User.findOne({email: email});
+        if (foundUser) { 
+            return res.status(403).send({error: "This email address is already being used."});
+    }
+
+
+        //If no, then create new user.
         const newUser = new User({
             email: email,
             password: password
         });
         await newUser.save();
 
+
+        //If yes, then respond with token.
         res.json({ user:"Created"});
     },
 
     signIn: async (req, res, next) => {
-        console.log('UsersController.signIn() called!');
+        // console.log('UsersController.signIn() called!');
     },
 
     accessGranted: async (req, res, next) => {
-        console.log('UsersController.acessGranted() called!');
+        // console.log('UsersController.acessGranted() called!');
     },
 };
