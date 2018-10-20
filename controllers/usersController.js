3da9a63dffd671fc18416cc0d5660859c73807f9
Belle <bebold.bebelle@gmail.com>
2018-10-20 16:30:44 -0400

    Worked on the implementation of the JWT involving creationg of the config file.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,21 @@
+const JWT = require("jsonwebtoken");
 const User = require("../models/user");
+const {JWT_SECRET} = require("../config");
+
+signToken = User => {
+    return JWT.sign(
+        {
+        iss: "BoatOuter",
+        sub: User._id,
+        iat:new Date().getTime(),
+        exp: new Date().setDate( new Date().getDate() + 1) //Current time + 1 day ahead.
+        }, 
+        JWT_SECRET);
+}
 
 module.exports = {
+
+    /* SIGNUP CONTROLLER */
     signUp: async (req, res, next) => {
         // console.log('UserController.signUp() called!');
 
@@ -10,10 +25,10 @@ module.exports = {
         // const password = req.value.body.password
 
         //Check if there is a user with the same email.
-        const foundUser = await User.findOne({email: email});
-        if (foundUser) { 
-            return res.status(403).send({error: "This email address is already being used."});
-    }
+        const foundUser = await User.findOne({ email: email });
+        if (foundUser) {
+            return res.status(403).send({ error: "This email address is already being used." });
+        }
 
 
         //If no, then create new user.
@@ -23,15 +38,32 @@ module.exports = {
         });
         await newUser.save();
 
+          // Generate the token
+          const token = signToken(newUser);
+
+        // const token = JWT.sign(
+        //     {
+        //         iss: "BoatOuter",
+        //         sub: newUser._id,
+        //         iat: new Date().getTime(),
+        //         exp: new Date().setDate(new Date().getDate() + 1) //Current time + 1 day ahead.
+        //     },
+        //     "boatouterauthentication");
 
         //If yes, then respond with token.
-        res.json({ user:"Created"});
+        res.status(200).json({ token: token });
+
     },
 
+
+
+    /* SIGNIN CONTROLLER */
     signIn: async (req, res, next) => {
         // console.log('UsersController.signIn() called!');
     },
 
+
+    /* ACCESS GRANTED CONTROLLER */
     accessGranted: async (req, res, next) => {
         // console.log('UsersController.acessGranted() called!');
     },
