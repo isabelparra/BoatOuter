f18dd8d4f1fb17e23a6916f25c22e7a2c293dae3
Belle <bebold.bebelle@gmail.com>
2018-10-21 18:42:57 -0400

    Passport is set up and test successfully.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
-// const router = require("express").Router();
 const router = require('express-promise-router')();
+// const router = express.Router;
+//The line above is uneccessary when using express-promise-router
 const passport = require("passport");
 const configPassport = require("../../passport");
 
-const {validateBody, schemas } = require('../../helpers/routesHelpers')
+
+const {validateBody, schemas } = require('../../helpers/routesHelpers');
 const UserController = require('../../controllers/usersController');
 
 router.route('/signup')
@@ -12,8 +14,8 @@ router.route('/signup')
 
 router.route('/signin')
     .post(UserController.signIn);
-    
-    router.route('/secret')
-    .get(passport.authenticate("jwt", {session: false}), UserController.secret);
+
+router.route('/accessgranted')
+    .get(passport.authenticate("jwt", {session: false}), UserController.accessGranted);
 
 module.exports = router;
